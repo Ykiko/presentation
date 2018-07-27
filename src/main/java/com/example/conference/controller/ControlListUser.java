f9bc6b8adb0fa043064813e21b6645efdccf62f5
grigory-yki <z.d.0.r.0.v.g.k@gmail.com>
2018-07-27 19:54:16 +0500

    функция регистрации User реализована

--- src/main/java/com/example/conference/controller/ControlListUser.java
+++ src/main/java/com/example/conference/controller/ControlListUser.java
@@ -1,4 +1,27 @@
 package com.example.conference.controller;
 
+import com.example.conference.entity.User;
+import com.example.conference.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class ControlListUser {
+
+    private Repository repository;
+
+    @Autowired
+    public ControlListUser(Repository repository){
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = {"/listOfUser/{id}"}, method = RequestMethod.GET)
+    public String listOfUser(@PathVariable ("id") Long id, Model model) {
+        model.addAttribute("user", repository.findById(id));
+        return "listOfUser";
+    }
 }
