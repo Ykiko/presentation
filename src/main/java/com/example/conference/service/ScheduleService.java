fb606ce334f04c41105f714e0a5ba4789033bf7a
grigory-yki <z.d.0.r.0.v.g.k@gmail.com>
2018-09-25 18:09:20 +0500

    вернул на место проверку на повтор записи Listener'a и поправил https://github.com/Ykiko/presentation/pull/1#discussion_r220075182; протестировано

--- src/main/java/com/example/conference/service/ScheduleService.java
+++ src/main/java/com/example/conference/service/ScheduleService.java
@@ -5,27 +5,40 @@ import com.example.conference.entity.User;
 import com.example.conference.exception.NotFoundException;
 import com.example.conference.repository.PresentationRepository;
 import com.example.conference.repository.UserRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@ConfigurationProperties(prefix = "error.message")
+@Data
 public class ScheduleService {
 
     private final PresentationRepository presentationRepository;
     private final UserRepository userRepository;
+    private String notFound;
 
     public void audition(Long id) throws NotFoundException {
 
         Presentation schedule = presentationRepository.findById(id).orElseThrow(NotFoundException::new);
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            String username = ((UserDetails) principal).getUsername();
+
+        String username = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).
+                filter(o -> o instanceof UserDetails).map(o -> (UserDetails)o).map(UserDetails::getUsername).orElseThrow(NotFoundException::new);
+
             User user = userRepository.findByUsername(username).orElseThrow(NotFoundException::new);
+
+            if(schedule.getListeners().contains(user)) {
+                throw new NotFoundException(notFound);
+            }
             schedule.getListeners().add(user);
             presentationRepository.save(schedule);
-        }
+
     }
 }
